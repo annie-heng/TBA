933d17e58564835a4835eaa5bed94efd03a0ad98
Matthieu <matthieu.thillay@edu.esiee.fr>
2025-11-24 16:41:36 +0000

    ajout poids max2

--- game.py
+++ game.py
@@ -132,7 +132,7 @@ class Game:
 
         #Setup item player
         self.player.inventory["money"] = money
-        player.current_weight += money.weight
+        self.player.current_weight += money.weight
 
     # Play the game
     def play(self):
